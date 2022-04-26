7d91b8d960bd8fcc246cd6400e6e84f36fe904eb
tsubasamaru <83381264+tsubasamaru@users.noreply.github.com>
2022-04-26 23:55:34 +0900

    Add files via upload

---
+++ browser_practice.py
@@ -0,0 +1,23 @@
+import tkinter
+import webbrowser
+
+class Click(tkinter.Button):
+    def __init__(self, title, url, master = None):
+        super().__init__(master, text = title, width = 15, command = self.click_button)
+        self.url = url
+    def click_button(self):
+        return webbrowser.open(self.url)
+
+if __name__ == '__main__':
+    list = [
+        ['Arsenal', 'https://www.arsenal.com'], 
+        ['Barcelona', 'https://www.fcbarcelona.jp/ja/']
+    ]
+
+root = tkinter.Tk()
+for info in list:
+    btn = Click(*info)
+    btn.pack()
+root.mainloop()
+
+
